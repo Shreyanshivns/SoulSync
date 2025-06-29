f93f820bd517afba3735b7cc3972458e0a4ac7ad
Shreyanshi singh <117850967+Shreyanshivns@users.noreply.github.com>
2025-06-30 01:40:19 +0530

    Added complete project files

---
+++ dist/js/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAy8LQsCRL49Nmoe0Fm3YR7RCxy9N69TPo",
+  authDomain: "soulsync-d2e40.firebaseapp.com",
+  projectId: "soulsync-d2e40",
+  storageBucket: "soulsync-d2e40.appspot.com",
+  messagingSenderId: "662234761",
+  appId: "1:662234761:web:26a05113517f9f83030858",
+  measurementId: "G-GMLQ3WTDX8"
+};
+export default firebaseConfig;
